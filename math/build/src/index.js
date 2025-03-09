f547eda4b84c39dbb98897bbf9283f0ccaf5b92a
Ashish Kumar Mohan <ashishkumarmohan5@gmail.com>
2025-03-09 18:54:08 +0530

    Delete math/build/src directory

--- math/build/src/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const countNumberOfDigits_2 = require("./countNumberOfDigits");
-console.log((0, countNumberOfDigits_2.countNumberOfDigits_1)(69420)); // O(1)
-console.log((0, countNumberOfDigits_2.countNumberOfDigits_logN)(69420)); //O()
-console.log((0, countNumberOfDigits_2.reverse)(69420));
-//# sourceMappingURL=index.js.map
